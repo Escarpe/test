5ac144950b8369cc3e4f3eabe85e626b609a720f
Escarpe <rudenko.max1@gmail.com>
2018-06-04 03:48:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var sheets = google.sheets('v4');
+var json = require('express-json');
+
+var data = fs.readFileSync('./credentials.json')
+
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets']; 
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'appsactivity-nodejs-quickstart.json';
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/views'));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(request, response){
+     
+    response.render("index");
+});
+
+app.post('/result', function(req, res) {
+    
+
+ 	 response = {
+            first_name : req.body.first_name,
+            last_name : req.body.last_name,
+            telephone : req.body.telephoneNumber,
+            email : req.body.email,
+            };
+        
+    
+    
+    var values = [[response.first_name, response.last_name, response.telephone, response.email]];
+
+	appendValues(data,values);
+
+
+    res.redirect('/');
+});
+
+
+
+
+app.listen(3000, function () {
+  console.log('Застосунок прослуховує 3000-ий порт!');
+});
+
+
+app.get('/view', function(req, res) {
+    
+
+	getValues(data,res);
+	
+
+
+});
+
+
+
+function appendValues(data,values) {
+ 
+    var auth = authorize(data);
+
+    var body = {
+      values: values
+    };
+    sheets.spreadsheets.values.append({
+      auth: auth,
+       spreadsheetId: '1O-BEh28VkNgoWhpjQls2clC8lmpP1yTBpcO68A0VGmY',
+       range: 'A2:D1000',
+       valueInputOption: 'USER_ENTERED',
+       resource: body
+    }, function(err, result) {
+      if(err) {
+        // Handle error
+        console.log(err);
+      } else {
+        console.log('%d cells updated.', result.updatedCells);
+      }
+    });
+}
+function getValues(data,res) {
+
+	var auth = authorize(data);
+  var request = {
+    auth: auth,
+    spreadsheetId: '1O-BEh28VkNgoWhpjQls2clC8lmpP1yTBpcO68A0VGmY',
+    range: 'A2:D1000',
+  };
+	sheets.spreadsheets.values.get(request, function(err, response) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+   	res.render("view",{
+    values : response.values,
+    n : 0,
+    i : 1
+  });
+ 	
+  });
+;}
+	
+ 
+  
+
+
+function authorize(credentials) {
+	credentials = JSON.parse(data);
+
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oauth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+
+    var token = fs.readFileSync(TOKEN_PATH);
+    oauth2Client.credentials = JSON.parse(token);
+    var authClient = oauth2Client;
+
+    return authClient;
+}
+
+
